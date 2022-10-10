e6b3259e985fc4a795178a5471e05d7ff9cfe417
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-10-10 00:40:18 -0300

    Add cors and express with typescript in project

--- MyApi/src/shared/http/server.ts
+++ MyApi/src/shared/http/server.ts
@@ -1,2 +1,12 @@
-export const first = "Olá Mundo!";
-console.log(first);
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.get('/', (request,response) => {
+    return response.json({message:"Olá Dev"});
+});
+
+app.listen(3000,()=> console.log("Rota Funcionando"));
