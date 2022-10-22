6dcd3c5a0278d8a0214cc73b74d76b43d7f597d3
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-10-22 20:44:52 -0300

    Add Errors Midleware

--- MyApi/src/shared/http/routes/index.ts
+++ MyApi/src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
+import { AppError } from "@shared/Errors/AppError";
 import { Router } from "express";
 const routes = Router();
 
 
 routes.get("/", (request, response) => {
+    throw new AppError("Acesso Negado");
     return response.json({ message: "Olá Dev" });
 });
 
--- MyApi/src/shared/http/server.ts
+++ MyApi/src/shared/http/server.ts
@@ -1,14 +1,30 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction } from "express";
 import "express-async-errors";
 import cors from "cors";
 import { routes } from "./routes";
+import { AppError } from "@shared/Errors/AppError";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+app.use((error : Error, request : Request, response : Response, next : NextFunction) => {
+    if (error instanceof(AppError)){
+        return response.status(error.statusCode).json({
+            status: "Error",
+            message: error.message
+        })
+    }
+    console.log(error);
+    if (error instanceof(AppError)){
+        return response.status(500).json({
+            status: "Error",
+            message: "Internal Server Error"
+        })
+    }
+})
 
 app.listen(process.env.PORT, () =>
     console.log(`Logado na porta ${process.env.PORT}`),
