1a4a0d2f6dba8de72db1c1309c5c791aece9761f
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-10-27 20:47:27 -0300

    add roles routes

--- MyApi/src/shared/http/routes/index.ts
+++ MyApi/src/shared/http/routes/index.ts
@@ -1,12 +1,12 @@
-import { AppError } from "@shared/Errors/AppError";
 import { Router } from "express";
+import { rolesRouter } from "@roles/http/routes/roles.routes";
 const routes = Router();
 
 
 routes.get("/", (request, response) => {
-    throw new AppError("Acesso Negado");
     return response.json({ message: "Olá Dev" });
 });
 
+routes.use("/roles",rolesRouter);
 
 export {routes};
