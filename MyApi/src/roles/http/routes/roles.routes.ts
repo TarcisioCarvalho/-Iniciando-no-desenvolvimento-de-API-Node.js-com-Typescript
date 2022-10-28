1739d2b428bacdbdc443c7e265e382fa5213bff9
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-10-28 16:49:00 -0300

    add role Entity

--- MyApi/src/roles/http/routes/roles.routes.ts
+++ MyApi/src/roles/http/routes/roles.routes.ts
@@ -1,16 +1,18 @@
+import { Role } from "@roles/entities/Role";
 import { Router } from "express";
-import {v4 as uuidv4} from 'uuid';
+
 const rolesRouter = Router();
 
-const roles = [];
+const roles:Role[] = [];
 
 rolesRouter.post("/",(request,response) => {
     const {name} = request.body;
-    const role = {
-        id: uuidv4(),
+    const role = new Role()
+    Object.assign(role,  {
         name: name,
         created_at: new Date()
-    }
+    })
+
     roles.push(role);
 
     return response.status(201).json(role);
