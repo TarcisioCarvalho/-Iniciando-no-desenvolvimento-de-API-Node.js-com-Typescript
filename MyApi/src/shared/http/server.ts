3a42ebc0e7a40f350bb0fb0abceb584c95c5c6e7
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-11-03 17:19:35 -0300

    Install and config swagger

--- MyApi/src/shared/http/server.ts
+++ MyApi/src/shared/http/server.ts
@@ -1,14 +1,16 @@
 import "dotenv/config";
 import express, { NextFunction } from "express";
 import "express-async-errors";
+import swaggerUI from "swagger-ui-express";
 import cors from "cors";
 import { routes } from "./routes";
 import { AppError } from "@shared/Errors/AppError";
+import swaggerFile from "../../swagger.json";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use("/docs",swaggerUI.serve,swaggerUI.setup(swaggerFile));
 app.use(routes);
 app.use((error : Error, request : Request, response : Response, next : NextFunction) => {
     if (error instanceof(AppError)){
