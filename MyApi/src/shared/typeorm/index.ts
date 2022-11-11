4748501be618ed4960b7bc318ee4deef10296588
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-11-11 17:20:41 -0300

    Refactor Role Entity

--- MyApi/src/shared/typeorm/index.ts
+++ MyApi/src/shared/typeorm/index.ts
@@ -1,3 +1,4 @@
+import { Role } from "@roles/entities/Role";
 import { DataSource } from "typeorm";
 import { CreateRolesTable1668111256449 } from "./migrations/1668111256449-CreateRolesTable";
 
@@ -5,6 +6,6 @@ import { CreateRolesTable1668111256449 } from "./migrations/1668111256449-Create
 export const dataSource = new DataSource({
     type:"sqlite",
     database:"./db.sqlite",
-    entities:[],
+    entities:[Role],
     migrations:[CreateRolesTable1668111256449]
 });
