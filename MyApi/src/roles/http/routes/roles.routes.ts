b4c48c79edbfa49a03ef3bed13629765cb24993a
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-11-15 11:03:32 -0300

    Create use case show role

--- MyApi/src/roles/http/routes/roles.routes.ts
+++ MyApi/src/roles/http/routes/roles.routes.ts
@@ -3,6 +3,7 @@ import { roleController } from "@roles/useCases/createRole";
 import { CreateRoleController } from "@roles/useCases/createRole/CreateRoleController";
 import { listRolesController } from "@roles/useCases/listRoles";
 import { ListRolesController } from "@roles/useCases/listRoles/ListRolesController";
+import { showRoleController } from "@roles/useCases/showrole";
 import { Router } from "express";
 
 const rolesRouter = Router();
@@ -14,6 +15,10 @@ rolesRouter.post("/",(request,response) => {
 
 rolesRouter.get("/",(request,response) => {
     listRolesController.handle(request,response);
+});
+
+rolesRouter.get("/:id",(request,response) => {
+  return  showRoleController.handle(request,response);
 });
 
 export {rolesRouter};
---
+++ MyApi/src/roles/useCases/showrole/ShowRoleUseCase.ts
@@ -0,0 +1,17 @@
+import { Role } from "@roles/entities/Role";
+import { RolesRepository } from "@roles/repositories/RolesRepository";
+import { AppError } from "@shared/Errors/AppError";
+
+type ShowRoleParams = {
+    id:string;
+}
+
+export class ShowRoleUseCase{
+    constructor(private rolesRepository: RolesRepository){}
+
+    async execute({id}: ShowRoleParams): Promise<Role>{
+        const role = await this.rolesRepository.findById(id);
+        if(!role) throw new AppError("Role Not Found",404);
+        return role;
+    }
+}
